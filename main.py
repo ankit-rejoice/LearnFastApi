7cf860d79995eb3b1df109fe4bc8de0e1f451253
ankit <ankit271.rejoice@gmail.com>
2023-12-05 11:54:23 +0530

    learning request body

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 from enum import Enum
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 class ModelName(str,Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
 
+
+class Item(BaseModel):
+    name : str
+    description : str | None = None
+    price :float
+    tax : float | None = None
+
 app = FastAPI()
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
+
+@app.get('/items/{item_id}')
+async def items(item_id : int , needy: str| None = None):
+    return {"item_id": item_id,"needy": needy}
+
+
+@app.post("/items/")
+async def create_item(item:Item):
+    return item
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
@@ -22,11 +43,6 @@ async def get_model(model_name: ModelName):
 
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
 
-@app.get('/items/{item_id}')
-async def items(item_id : int , needy: str| None = None):
-    return {"item_id": item_id,"needy": needy}
+
